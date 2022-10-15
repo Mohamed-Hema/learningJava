52126ba650fdb0b2b60515b5146840b62d185e25
Mohamed-Hema <mohamedhema2017@gmail.com>
2022-10-15 17:26:33 +0200

    introducing loop

--- src/random.java
+++ src/random.java
@@ -1,9 +1,15 @@
-
+import java.util.Random;
 public class random {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random random = new Random();
+		
+		//int x = random.nextInt(7) + 1;
+		//double y = random.nextDouble();
+		
+		boolean z = random.nextBoolean();
+		System.out.println(z);
 	}
 
 }
---
+++ src/switch22.java
@@ -0,0 +1,25 @@
+
+public class switch22 {
+
+	public static void main(String[] args) {
+		String day = "pizza";
+		
+		switch(day) {
+		case "Friday": System.out.println("It is Friday!");
+		break;
+		case "Sunday": System.out.println("It is Sunday!");
+		break;
+		case "Monday": System.out.println("It is Monday!");
+		break;
+		case "Tuesday": System.out.println("It is Tuesday!");
+		break;
+		case "Wednesday": System.out.println("It is Wednesday!");
+		break;
+		case "Thursday": System.out.println("It is Thursday!");
+		break;
+		case "Saturday": System.out.println("It is Saturday!");
+		break;
+		default: System.out.println("That's not a day!");
+		}
+	}
+}
